ce7702780caf3316f455166c4d03497892c9ad63
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-14 15:03:30 +0200

    add followus App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Component/Navbar';
 import Hero from './Component/Hero';
 import TrendingProducts from './Component/TrendingProducts';
 import SilonJournal from './Component/SilonJournal';
+import FollowUs from './Component/FollowUs';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
        <Hero />
       <TrendingProducts />
       <SilonJournal />
+      <FollowUs />
     </div>
   );
 }
