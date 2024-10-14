629fc0b6316fb3e560ba4f154ad54ee9718b4385
Sylwia Kruk <sylviaamj@gmail.com>
2024-10-14 15:17:50 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faPinterestP, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faApple, faAndroid } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -43,18 +44,18 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="md:col-span-2 relative text-left">
+          <div className="md:col-span-2 relative text-left flex flex-col items-start">
             <h1 className="text-base font-semibold mb-4">EXPERIENCE APP ON MOBILE</h1>
             <div className="flex justify-center border-b border-[#d6d8d9] pb-9">
-              <a href="#" className="flex items-center justify-center w-32 h-10 bg-white border rounded-md shadow-md relative mr-2"> {/* Zmniejszenie wysokości */}
-                <i className="text-[#ff5353] text-lg absolute left-2 top-1/2 transform -translate-y-1/2"></i>
-                <p className="text-black text-xs font-medium uppercase text-center ml-8"> {/* Zmniejszenie czcionki */}
+              <a href="#" className="flex items-center justify-center w-32 h-10 bg-white border rounded-md shadow-md relative mr-2">
+                <FontAwesomeIcon icon={faAndroid} className="text-[#3ddc84] text-lg absolute left-2 top-1/2 transform -translate-y-1/2" />
+                <p className="text-black text-xs font-medium uppercase text-center ml-8">
                   <span className="block text-xs font-normal text-gray-600 mb-0">get it on</span>
                   Google Play
                 </p>
               </a>
               <a href="#" className="flex items-center justify-center w-32 h-10 bg-[#ff5353] text-white border rounded-md shadow-md relative">
-                <i className="text-white text-lg absolute left-2 top-1/2 transform -translate-y-1/2"></i>
+                <FontAwesomeIcon icon={faApple} className="text-white text-lg absolute left-2 top-1/2 transform -translate-y-1/2" /> 
                 <p className="text-white text-xs font-medium uppercase text-center ml-8">
                   <span className="block text-xs font-normal text-white mb-0">get it on</span>
                   App Store
@@ -62,28 +63,29 @@ const Footer = () => {
               </a>
             </div>
 
-            <h1 className="text-base font-semibold mb-4">ONLINE SHOPPING</h1>
+            <h1 className="text-base font-semibold mb-4 mt-6">FOLLOW US</h1>
             <div className="flex space-x-4 justify-center">
-              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md">
-                <FontAwesomeIcon icon={faFacebookF} className="text-black" />
+              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md hover:bg-[#ff5353] hover:text-white transition duration-300">
+                <FontAwesomeIcon icon={faFacebookF} className="text-black hover:text-white" />
               </a>
-              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md">
-                <FontAwesomeIcon icon={faTwitter} className="text-black" />
+              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md hover:bg-[#ff5353] hover:text-white transition duration-300">
+                <FontAwesomeIcon icon={faTwitter} className="text-black hover:text-white" />
               </a>
-              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md">
-                <FontAwesomeIcon icon={faPinterestP} className="text-black" />
+              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md hover:bg-[#ff5353] hover:text-white transition duration-300">
+                <FontAwesomeIcon icon={faPinterestP} className="text-black hover:text-white" />
               </a>
-              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md">
-                <FontAwesomeIcon icon={faInstagram} className="text-black" />
+              <a href="#" className="w-8 h-8 bg-white border flex items-center justify-center shadow-md hover:bg-[#ff5353] hover:text-white transition duration-300">
+                <FontAwesomeIcon icon={faInstagram} className="text-black hover:text-white" />
               </a>
             </div>
           </div>
         </div>
       </div>
 
+      <hr className="my-6 border-t border-[#d6d8d9]" />
+
       <div className="py-5 text-center text-gray-500 mb-0">
-        <p className="mb-0">© 2024 Company. All Rights Reserved.</p>
-        <a href="#" className="text-[#ff5353]">Privacy Policy</a>
+        <p className="mb-0">Copyright © 2024. All Rights Reserved.</p>
       </div>
     </footer>
   );
