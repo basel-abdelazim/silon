83d204101d0bf46a673e4623e74b7198c835049a
waleedsinger <waleedwalker@icloud.com>
2024-10-14 15:35:44 +0200

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Component/Navbar';
 import Hero from './Component/Hero';
+import FeaturedProducts from './Component/FeaturedProducts';  
+import Popular from './Component/Popular';
 import TrendingProducts from './Component/TrendingProducts';
 import SilonJournal from './Component/SilonJournal';
 import FollowUs from './Component/FollowUs';
@@ -10,6 +12,8 @@ function App() {
     <div>
        <Navbar />  
        <Hero />
+          <FeaturedProducts />
+                <Popular /> 
       <TrendingProducts />
       <SilonJournal />
       <FollowUs />
